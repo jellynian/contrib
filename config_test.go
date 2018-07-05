e4e9dd1a0c5f3270d9222cc324de0a6ac01f026b
jelly <jellynian6@gmail.com>
2018-07-05 11:55:29 +0800

    add test case for config,switch dep to manager vender

---
+++ config_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+
+	"bitbucket.org/jellynian/contrib/config"
+)
+
+func TestConfig(t *testing.T) {
+	conf := config.Default()
+	t.Log("read result is :", conf.Get("user").String())
+
+	t.Log("read result is :", conf.Get("global.addr").String())
+}
+
+func TestConfigOnly(t *testing.T) {
+	conf := config.Default()
+	t.Log("read result is :", conf.Get("user").String())
+}
